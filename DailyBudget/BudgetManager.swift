c17606c3874e042ebd206b8f7cc9358400914f6a
Jonathan T. Miles <jonathan.t.miles@gmail.com>
2020-02-03 21:51:12 -0800

    outline

---
+++ DailyBudget/BudgetManager.swift
@@ -0,0 +1,33 @@
+//
+//  BudgetManager.swift
+//  DailyBudget
+//
+//  Created by Jonathan T. Miles on 2/3/20.
+//  Copyright © 2020 Jonathan T. Miles. All rights reserved.
+//
+
+import Foundation
+
+class BudgetManager {
+    /*
+     
+     values for setting budget
+     methods for reporting current budget
+     would love an elastic set of tools for declaring new tags and expanding new "columns" to work with
+     ability to save to disk and load from there
+     
+     hard-coding values:
+     food, groceries, transportation, discretionary
+     starting totals
+     day calculator for decrementing or incrementing available budget
+     array of line item models (description, date, category enum, value)
+     
+     is there a spreadsheet or table framework?
+     
+     outside of hard-coding,
+     budget category models with label and budget value types, and maybe a time length (daily, monthly)
+     separate model controller for fetching and herding these models and their values
+     a new controller (budget constructor?) for synthesizing and presenting all these types (just as a view manager), with each model manager being rewritten to have to be passed in a whole budget object which is then operated on (post-singleton, obviously)
+     
+     */
+}
---
+++ DailyBudget/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DailyBudget
+//
+//  Created by Jonathan T. Miles on 2/3/20.
+//  Copyright © 2020 Jonathan T. Miles. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
